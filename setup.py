36125bf7ce239b1100bd31f8dcbc0ee0f7947547
Francisco Carvajal Ossa <fraco_oxza@outlook.com>
2021-03-02 19:44:00 -0300

    first commit

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="keysStorage",
+    version="0.2",
+    package=find_packages(),
+    include_packages_data=True,
+    install_requires=[
+        "click", "pycrypto"
+    ],
+    entry_points="""
+        [console_scripts]
+        keys=keys.scripts:cli
+    """
+)
